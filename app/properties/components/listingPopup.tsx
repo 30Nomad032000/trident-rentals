7d35e1eec7f6151478986ab2d1c6be86fa059660
30Nomad032000 <ebin.john76@gmail.com>
2024-11-05 16:40:28 +0530

    fix: filter popup fix

--- app/properties/components/listingPopup.tsx
+++ app/properties/components/listingPopup.tsx
@@ -47,7 +47,7 @@ export default function PropertyModal({
   return (
     <Dialog open={isOpen} onOpenChange={(value: boolean) => handleClose(value)}>
       <DialogTitle className="hidden">Property Details</DialogTitle>
-      <DialogContent className="px-12 py-12 md:max-w-6xl bg-gray-100">
+      <DialogContent className="px-12 py-12 md:max-w-6xl max-h-[90vh] bg-gray-100">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-8">
           <div className="lg:pr-6 lg:border-r h-full">
             <CarouselOrientation />
