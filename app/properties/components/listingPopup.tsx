f2c0bca73292a67f5a554017ce2afb65e0876320
30Nomad032000 <ebin.john76@gmail.com>
2024-12-16 04:47:18 +0530

    fix: listing card responsive fix

--- app/properties/components/listingPopup.tsx
+++ app/properties/components/listingPopup.tsx
@@ -68,13 +68,14 @@ export default function PropertyModal({
         onOpenChange={(value: boolean) => handleClose(value)}
       >
         <DialogTitle className="hidden">Property Details</DialogTitle>
+
         <DialogContent
           onInteractOutside={(e) => {
             e.preventDefault();
           }}
-          className="px-12 py-12 md:max-w-6xl max-h-[90vh] bg-gray-100"
+          className="px-12 py-12 md:max-w-6xl h-[70vh] bg-gray-100"
         >
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-8 overflow-y-auto overflow-x-hidden">
             <div className="lg:pr-6 lg:border-r h-full">
               <CarouselOrientation
                 property={property}
